7ae394b158919c031ced9130ea07c73983a3d6ad
pablo <pablo93bio@gmail.com>
2022-01-25 15:54:01 +0100

    comentarios en model tables API

--- API/ApiRest/models/table.js
+++ API/ApiRest/models/table.js
@@ -1,4 +1,16 @@
 const mongoose = require("mongoose");
+
+/*
+    -> name
+    -> status (ocupada o libre)
+    -> numero_mesa (posicion en el grid, valor auto)
+    -> id_cliente (empresa, no haria falta porque la zona ya lo lleva)
+    -> id_zona (_id de zona en la que va, cambiar cuenta de la mesa con una mesa de otra zona?)
+    -> comensales (no requerido, default 1)
+    -> cuenta (deberia ser una especie de hashMap de
+        nombre_producto : array con valores tipo: cantidad, id_producto, precio)
+*/
+
 const tableSchema = mongoose.Schema({
     name:{
         type:String,
